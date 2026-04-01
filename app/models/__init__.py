13837a5be262d66e25156b40e707824221bee3b9
nezxenka <kuznetsovaleks2009@mail.ru>
2026-04-02 02:01:41 +0300

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.models.merchant import Merchant
-from app.models.transaction import Transaction, TransactionStatus, TransactionType
-from app.models.webhook import WebhookEndpoint, WebhookLog
--- app/schemas/__init__.py
+++
@@ -1,2 +0,0 @@
-from app.schemas.merchant import MerchantCreate, MerchantLogin, MerchantResponse, ApiKeyResponse
-from app.schemas.transaction import TransactionCreate, TransactionResponse
